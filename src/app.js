ee378d10ca8d9603fde57a7e746b302f754e1503
Henry Acosta <acosta.henry@gmail.com>
2018-06-29 15:46:59 -0400

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Link, NavLink } from "react-router-dom";
+import 'normalize.css/normalize.css'
+import './styles/styles.scss'
+
+
+
+ReactDOM.render(<p>My App</p>, document.getElementById('app'));
